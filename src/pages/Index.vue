326760c8c32d3fe5bebf1f506b006530eecac16c
raul.hess <raul.hess@preambulo.com.br>
2018-11-27 11:02:24 -0200

    commit incial

---
+++ src/pages/Index.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-page class="column fit">
+    <div class="col-2 bg-grey-2 ">
+      Jumbotron
+    </div>
+    <div class="col row">
+        <div class="col-4 bg-blue-5">
+          Menu 1
+        </div>
+        <div class="col-4 bg-red-5">
+          Menu 2
+        </div>
+        <div class="col-4 bg-green-5">
+          Menu 3
+        </div>
+        <div class="col-4 bg-yellow-5">
+          Menu 4
+        </div>
+        <div class="col-4 bg-orange-5">
+          Menu 5
+        </div>
+        <div class="col-4 bg-purple-5">
+          Menu 6
+        </div>
+    </div>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
